9c9b8202ecd34c1e3be0e8ed13b8660dba244525
Jason <jason.onggo@tempatkerja.com>
2022-12-21 08:25:47 +0700

    add search bar

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,64 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+import { ChakraProvider, Link, LinkBox, LinkOverlay, Text } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import { useState } from 'react'
 
 export default function Home() {
+  const router = useRouter()
+
+  const [searchValue, setSearchValue] = useState('')
+  const [dictValue, setDictValue] = useState('')
+
+  const handleChange = (event) => {
+    setSearchValue(event.target.value)
+    if (event.target.value != '') {
+      fetchData(event.target.value)
+    }
+  }
+
+  const fetchData = async (query) => {
+    console.log('fetching', `https://somas.godata.id/search?query=${query}`)
+    await fetch(`https://somas.godata.id/search?query=${query}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setDictValue(data)
+    })
+  }
+
+  const listItems = Object.keys(dictValue).map((key) => 
+  <LinkBox key={key} w='100%' p='2' borderWidth='1px' rounded='md'>
+    <LinkOverlay href={`?symbol=${key}`}>
+      <Text>{key} {dictValue[key]}</Text>
+    </LinkOverlay>
+  </LinkBox>)
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ChakraProvider>
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <main className={styles.main}>
+          <Input value={searchValue} onChange={handleChange}/>
+          {searchValue != '' && dictValue != '' && 
+            <>
+              {listItems}
+            </>}
+          <h1>Symbol {router.query.symbol || "ICBP"}</h1>
+        </main>
+      </div>
+    </ChakraProvider>
   )
+}
+
+export async function getServerSideProps(context) {
+  console.log(context.query.symbol)
+  return {
+    props: {}
+  }
 }
